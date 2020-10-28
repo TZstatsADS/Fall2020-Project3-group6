d00912a54f38c1cbfdf82d3943df64ed31b6fd45
mengyaoo <61811512+mengyaoo@users.noreply.github.com>
2020-10-27 22:42:01 -0400

    update feature selection

--- lib/feature_selection.R
+++ lib/feature_selection.R
@@ -1,11 +1,31 @@
+if(!require("leaps")){
+  install.packages("leaps")
+}
+
+if(!require("glmnet")){
+  install.packages("glmnet")
+}
+
+if(!require("caret")){
+  install.packages("caret")
+}
+
+if(!require("randomForest")){
+  install.packages("randomForest")
+}
+
 library(leaps)
 library(glmnet)
 library(caret)
 library(randomForest)
-getwd()
-setwd("~/Documents/GitHub/Fall2020-Project3-group6/lib/")
+
+#getwd()
+#setwd("~/Documents/GitHub/Fall2020-Project3-group6/lib/")
+
 load("../output/feature_train.RData")
-# Best Subset Selection
+load("../output/feature_test.RData")
+
+# Best Subset Selection ----------------------------------------------------------------------
 # best_fit <- regsubsets(label ~ ., data = dat_train)
 
 # Forward Stepwise Selection -----------------------------------------------------------------
@@ -19,7 +39,7 @@ save(forward_feature, file="../output/forward_feature.RData")
 load("../output/forward_feature.RData")
 
 
-# Backward Stepwise Selection-------------------------------------------------------------------------
+# Backward Stepwise Selection---------------------------------------------------------------
 # backward_fit <- regsubsets(label ~ ., data = dat_train, method = "backward",nvmax= (ncol(dat_train)-1))
 # backward_summ = summary(backward_fit)
 # best.size = which.max(backward_fit$adjr2)
@@ -45,17 +65,10 @@ lasso_feature = as.matrix(coef(lasso.mod))
 length(lasso_feature[lasso_feature != 0])
 save(lasso_feature, file="../output/lasso_feature.RData")
 
-
 # PCA ---------------------------------------------------------------------
 pca <- preProcess(x=dat_train,method="pca",thresh=0.99)
 pca_feature <- predict(pca,dat_train[,-6007])
 save(pca_feature, file="../output/pca_feature.RData")
-
-# RFE (to be finished)-------------------------------------------------------------------------
-rfe_fit = rfe(dat_train[,-6007], dat_train[,6007], sizes = seq(140,320,20),
-              rfeControl = rfeControl(functions = lmFuncs, rerank = TRUE, number = 10))
-
-save(rfe_fit, file="../output/feature_selection.RData")
 
 # Random Forest-------------------------------------------------------------------------
 rf_fit = randomForest(label~.,data=dat_train,importance = TRUE)
@@ -65,12 +78,6 @@ save(rf_fit, file="../output/feature_selection.RData")
 rf_feat <- as.data.frame(importance(rf_fit))
 rf_names <- rownames(rf_feat[order(rf_feat$MeanDecreaseAccuracy), ])[1:length(forward_feature)]
 
-
-
-
-varImpPlot(rf_fit)
-importance(rf_fit)
-
 yhat.rf = predict(rf_fit,newdata = dat_test)
-mean((yhat.rf-dat_test$label)^2)
+mean(yhat.rf == dat_test$label)
 
