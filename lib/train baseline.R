b0c213004a16c5d1fd99c86cfce0e4807194f4dd
Yiqi Lei <71394679+aaalicelei@users.noreply.github.com>
2020-11-02 19:12:20 +0800

    Upload cv, train, test for baseline

---
+++ lib/train baseline.R
@@ -0,0 +1,20 @@
+###########################################################
+### Train a classification model with training features ###
+###########################################################
+
+
+
+
+train <- function(features, labels, w=NULL, n.trees, shrinkage){
+model <- gbm.fit(x=features,
+                 y=labels,
+                 distribution = "multinomial",    #"bernoulli",   #"multinomial", #logistic regression for 0-1 outcomes
+                 w=w,
+                 n.trees=n.trees,
+                 shrinkage=shrinkage,
+                 verbose=F)
+return(model)
+}
+
+
+
