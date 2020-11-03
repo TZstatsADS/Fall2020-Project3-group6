a9b8df668b52d832090f7631baca7002c1c46ed1
Jiaqi Yuan <66074346+Vikkiyyy@users.noreply.github.com>
2020-11-02 22:48:42 -0500

    feature selection with edit

--- lib/feature_selection.R
+++ lib/feature_selection.R
@@ -20,7 +20,7 @@ library(randomForest)
 #getwd()
 #setwd("~/Documents/GitHub/Fall2020-Project3-group6/lib/")
 load("../output/feature_train.RData")
-
+load("../output/forward_feature.RData")
 # Best Subset Selection----------------------------------------------------------------
 # best_fit <- regsubsets(label ~ ., data = dat_train)
 
@@ -62,8 +62,11 @@ save(lasso_feature, file="../output/lasso_feature.RData")
 
 # PCA-------------------------------------------------------------------------
 pca <- preProcess(x=dat_train,method="pca",thresh=0.95)
-pca_feature <- predict(pca,dat_train[,-6007])
-save(pca_feature, file="../output/pca_feature.RData")
+pca_feature_train <- predict(pca,dat_train[,-6007])
+save(pca_feature_train, file="../output/pca_feature_train.RData")
+pca_feature_test <- predict(pca,dat_test[,-6007])
+save(pca_feature_test, file="../output/pca_feature_test.RData")
+
 
 # Random Forest-------------------------------------------------------------------------
 rf_fit = randomForest(label~.,data=dat_train,importance = TRUE)
