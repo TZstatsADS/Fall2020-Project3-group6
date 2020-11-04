a9b64d7b4e40034a3195de4192134b21527a8d25
mengyaoo <61811512+mengyaoo@users.noreply.github.com>
2020-11-04 08:40:36 -0500

    Update feature_selection.R

--- lib/feature_selection.R
+++ lib/feature_selection.R
@@ -21,6 +21,12 @@ library(randomForest)
 #setwd("~/Documents/GitHub/Fall2020-Project3-group6/lib/")
 load("../output/feature_train.RData")
 load("../output/forward_feature.RData")
+
+dat_train_rescale <- as.data.frame(lapply(dat_train[,-6007], rescale))
+dat_test_rescale <- as.data.frame(lapply(dat_test[,-6007], rescale))
+dat_train_rescale <- cbind(dat_train_rescale, new_col = dat_train[,6007])
+dat_test_rescale <- cbind(dat_test_rescale, new_col = dat_test$label)
+
 # Best Subset Selection----------------------------------------------------------------
 # best_fit <- regsubsets(label ~ ., data = dat_train)
 
@@ -33,6 +39,16 @@ forward_feature = names(forward_feature)
 length(forward_feature)
 save(forward_feature, file="../output/forward_feature.RData")
 load("../output/forward_feature.RData")
+
+# Forward Stepwise Selection(scale)-------------------------------------------------------------------------
+forward_fit <- regsubsets(label ~ ., data = dat_train_rescale, method = "forward",nvmax= (ncol(dat_train_rescale)-1))
+forward_summ = summary(forward_fit)
+best.size = which.max(forward_summ$adjr2)
+forward_feature = forward_summ$which[best.size,][forward_summ$which[best.size,] == TRUE][-1]
+forward_feature = names(forward_feature)
+length(forward_feature)
+save(forward_feature, file="../output/forward_feature.RData")
+load("../output/forward_feature_scale.RData")
 
 # Backward Stepwise Selection-------------------------------------------------------------------------
 # backward_fit <- regsubsets(label ~ ., data = dat_train, method = "backward",nvmax= (ncol(dat_train)-1))
@@ -60,6 +76,7 @@ lasso_feature = as.matrix(coef(lasso.mod))
 length(lasso_feature[lasso_feature != 0])
 save(lasso_feature, file="../output/lasso_feature.RData")
 
+
 # PCA-------------------------------------------------------------------------
 pca <- preProcess(x=dat_train[,-6007],method="pca",thresh=0.95)
 pca_feature_train <- predict(pca,dat_train)
@@ -67,16 +84,19 @@ save(pca_feature_train, file="../output/pca_feature_train.RData")
 pca_feature_test <- predict(pca,dat_test)
 save(pca_feature_test, file="../output/pca_feature_test.RData")
 
+# PCA(scale)-------------------------------------------------------------------------
+pca <- preProcess(x=dat_train_rescale[,-6007],method="pca",thresh=0.95)
+pca_feature_train <- predict(pca,dat_train_rescale)
+save(pca_feature_train, file="../output/pca_feature_train_scale.RData")
+pca_feature_test <- predict(pca,dat_test_rescale)
+save(pca_feature_test, file="../output/pca_feature_test_scale.RData")
+
 
 # Random Forest-------------------------------------------------------------------------
 rf_fit = randomForest(label~.,data=dat_train,importance = TRUE)
 save(rf_fit, file="../output/rf_feature.RData")
 
-# not sure 
 rf_feat <- as.data.frame(importance(rf_fit))
-rf_names <- rownames(rf_feat[order(rf_feat$MeanDecreaseAccuracy), ])[1:length(forward_feature)]
-
-yhat.rf = predict(rf_fit,newdata = dat_test)
-mean(yhat.rf == dat_test$label)
+rf_names <- rownames(rf_feat[order(rf_feat$MeanDecreaseAccuracy), ])
 
 
