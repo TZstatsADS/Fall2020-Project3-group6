fdbdc5370944391f9dbfa15ed84b752283896547
Yiqi Lei <71394679+aaalicelei@users.noreply.github.com>
2020-11-04 20:55:01 +0800

    update baseline and readme

--- README.md
+++ README.md
@@ -18,6 +18,13 @@ Term: Fall 2020
 	
 **Contribution statement**: ([default](doc/a_note_on_contributions.md)) All team members contributed equally in all stages of this project. All team members approve our work presented in this GitHub repository including this contributions statement. 
 
+Mengyao He: 
+Qinzhe Hu: 
+Yiqi Lei: Did the baseline model, collaborated on writing the main file (including final training and prediction part) with Yuwei.
+Yuwei Tong: 
+Jiaqi Yuan: 
+	
+
 Following [suggestions](http://nicercode.github.io/blog/2013-04-05-projects/) by [RICH FITZJOHN](http://nicercode.github.io/about/#Team) (@richfitz). This folder is orgarnized as follows.
 
 ```
--- lib/train_baseline.R
+++ lib/train_baseline.R
@@ -5,15 +5,15 @@
 
 
 
-train <- function(features, labels, w=NULL, n.trees, shrinkage){
-model <- gbm.fit(x=features,
-                 y=labels,
-                 distribution = "multinomial",    
-                 w=w,
-                 n.trees=n.trees,
-                 shrinkage=shrinkage,
-                 verbose=F)
-return(model)
+train_baseline <- function(features, labels, w=NULL, n.trees, shrinkage){
+  model <- gbm.fit(x=features,
+                   y=labels,
+                   distribution = "multinomial",    
+                   w=w,
+                   n.trees=n.trees,
+                   shrinkage=shrinkage,
+                   verbose=F)
+  return(model)
 }
 
 
